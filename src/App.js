a3c3a2d14265f2404ed9da018a4958b35c7e2144
Anne Chen <annechen.anne@gmail.com>
2019-03-16 09:10:29 -0700

    added cartoons.json

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import cartoons from "./cartoons.json";
 
 class App extends Component {
+  // Setting this.state.friends to the friends json array
+  state = {
+    cartoons
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        {this.state.cartoons.map(cartoon => (
+          <Card
+            id={cartoon.id}
+            key={cartoon.id}
+            name={cartoon.name}
+            image={cartoon.image}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
