eca7577249cdd3e04610e209763add54ca973561
Anne Chen <annechen.anne@gmail.com>
2019-03-17 14:19:50 -0700

    Updates

---
+++ precache-manifest.c914b60355d8eb5dde49f05eaca7cc8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f57e7f88393c997212e9",
+    "url": "/clicky_app/static/js/runtime~main.04086a56.js"
+  },
+  {
+    "revision": "b594deed246ccc747e20",
+    "url": "/clicky_app/static/js/main.d7248981.chunk.js"
+  },
+  {
+    "revision": "a252e227d3244d802167",
+    "url": "/clicky_app/static/js/2.31a730b4.chunk.js"
+  },
+  {
+    "revision": "b594deed246ccc747e20",
+    "url": "/clicky_app/static/css/main.97038a54.chunk.css"
+  },
+  {
+    "revision": "43be3b0ebbc13bddf1be6f8fcdfaba70",
+    "url": "/clicky_app/index.html"
+  }
+];
